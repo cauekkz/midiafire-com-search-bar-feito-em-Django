0e058aa69ae6e6d68ced9a3817863924f3bb4963
Cauê Vaccari <cauevaccari07@gmail.com>
2024-02-17 01:37:12 -0300

    edit File model e criando modelo message

--- shareWeb/fastFile/models.py
+++ shareWeb/fastFile/models.py
@@ -9,13 +9,18 @@ class User(AbstractUser):
 
 class File(models.Model):
     name = models.CharField(max_length=100)
-    postedBy = models.ForeignKey(User, related_name='posteds', on_delete=models.CASCADE,default='the account no longer exists')
+    postedBy = models.ForeignKey(User, related_name='posteds', on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
     postedAt = models.DateTimeField(auto_now_add=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, default='')
     userPermition = models.BooleanField(default=False)
     allowedUsers = models.ManyToManyField(User, related_name='allowedFiles')
     downloadsCount = models.IntegerField(default=0)
-    messages = models.ManyToManyField(User, related_name='myRequests')
 
-    
+class Message(models.Model):
+    caller = models.ForeignKey(User, related_name='sends', on_delete=models.CASCADE, null=True)
+    reciver = models.ForeignKey(User,related_name='messages',on_delete=models.CASCADE)
+    message = models.CharField(max_length=200)
+    request = models.BooleanField(default=False)
+    sendAt = models.DateTimeField(auto_now_add=True)
+
